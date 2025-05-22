1b73327ec666f6fed0090c1251a4800584a97c94
David Nintang <nintang48@gmail.com>
2025-05-21 23:59:44 -0700

    Update .gitignore to include .env and modify main.py to load environment variables using dotenv; added message handling for translation functionality.

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import getpass
 import os
+from dotenv import load_dotenv
 
-if not os.environ.get("OPENAI_API_KEY"):
-  os.environ["OPENAI_API_KEY"] = getpass.getpass("Enter API key for OpenAI: ")
+load_dotenv()
+
+if not os.getenv("OPENAI_API_KEY"):
+  os.getenv["OPENAI_API_KEY"] = getpass.getpass("Enter API key for OpenAI: ")
 
 from langchain.chat_models import init_chat_model
+from langchain_core.messages import HumanMessage, SystemMessage
 
 model = init_chat_model("gpt-4o-mini", model_provider="openai")
+
+messages = [
+  SystemMessage("Translate the following text from English to French"),
+  HumanMessage("Hi!")
+]
+
+print(model.invoke(messages))
