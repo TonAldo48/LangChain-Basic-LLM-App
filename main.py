6efa18cd8a6c8dea60d8132609763e0c233ece7c
David Nintang <nintang48@gmail.com>
2025-05-22 00:21:07 -0700

    Utilised system template and ChatPromptTemplate to create variable invocations.

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ if not os.getenv("OPENAI_API_KEY"):
 
 from langchain.chat_models import init_chat_model
 from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_core.prompts import ChatPromptTemplate
 
 model = init_chat_model("gpt-4o-mini", model_provider="openai")
 
@@ -21,5 +22,17 @@ model = init_chat_model("gpt-4o-mini", model_provider="openai")
 
 # print(model.invoke("Hello"))
 
-for token in model.stream("Hello"):
-   print(token.content, end="|")
+# for token in model.stream("Hello"):
+#    print(token.content, end="|")
+
+system_template = "Translate the following text from English to {language}"
+prompt_template = ChatPromptTemplate.from_messages([
+  ("system", system_template),
+  ("user", "{text}")
+])
+
+prompt = prompt_template.invoke({"language": "French", "text": "I love programming in Python"})
+
+# print(prompt)
+
+print(model.invoke(prompt).content)
